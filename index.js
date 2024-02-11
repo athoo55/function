0ceb13fd538a2d09d8156c3ad0f362880691a657
athoo55 <dhwdhwra@gmail.com>
2024-02-11 20:04:58 +0300

    add function

---
+++ index.js
@@ -0,0 +1,14 @@
+function addStudents(studentArray, ageArray) {
+    for (let i = 0; i < studentArray.length; i++) {
+
+    }
+        
+    
+
+    console.log(studentArray, ageArray);
+}
+let student = [{ name: "athoo" },{ name: "noor" },{ name: "aya" }]
+let age = [{ age: "20" }, { age: "22" }, { age: "24" }]
+
+
+addStudents(student, age);
